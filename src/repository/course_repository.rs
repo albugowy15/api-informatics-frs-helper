7964fe5b36a9aed981ab44be52423053c8c713cb
albugowy15 <kholidbughowi@gmail.com>
2024-01-07 18:44:22 +0700

    feat: implement course enpoint

--- src/repository/course_repository.rs
+++ src/repository/course_repository.rs
@@ -1,11 +1,53 @@
-// use crate::db::DbPool;
+use serde::Serialize;
+use sqlx::Row;
 
-// // pub struct CourseRepository<'a> {
-// //     db: &'a DbPool,
-// // }
+use crate::db::DbPool;
 
-// // impl<'a> CourseRepository<'a> {
-// //     pub fn new(db_connection: &'a DbPool) -> CourseRepository {
-// //         CourseRepository { db: db_connection }
-// //     }
-// // }
+pub struct CourseRepository<'a> {
+    db: &'a DbPool,
+}
+
+#[derive(Serialize)]
+pub struct Course {
+    pub id: String,
+    pub nama: String,
+    pub semester: i8,
+    pub sks: i8,
+}
+
+impl<'a> CourseRepository<'a> {
+    pub fn new(db_connection: &'a DbPool) -> CourseRepository {
+        CourseRepository { db: db_connection }
+    }
+
+    pub async fn get_courses(&self) -> Result<Vec<Course>, sqlx::Error> {
+        let mut courses = Vec::new();
+        let rows = sqlx::query("select m.id, m.name, m.semester, m.sks from Matkul m")
+            .fetch_all(self.db)
+            .await?;
+        rows.into_iter().for_each(|row| {
+            courses.push(Course {
+                id: row.get("id"),
+                nama: row.get("name"),
+                semester: row.get("semester"),
+                sks: row.get("sks"),
+            })
+        });
+        Ok(courses)
+    }
+
+    pub async fn get_course_by_id(&self, course_id: &String) -> Result<Course, sqlx::Error> {
+        let row =
+            sqlx::query("select m.id, m.name, m.semester, m.sks from Matkul m where m.id = ?")
+                .bind(course_id)
+                .fetch_one(self.db)
+                .await?;
+        let course = Course {
+            id: row.get("id"),
+            nama: row.get("name"),
+            semester: row.get("semester"),
+            sks: row.get("sks"),
+        };
+        Ok(course)
+    }
+}
--- src/services/course_service.rs
+++ src/services/course_service.rs
@@ -1,24 +1,88 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
-use axum::{extract::Query, Json};
-use serde::Serialize;
+use axum::{
+    extract::{Path, Query, State},
+    http::StatusCode,
+    Json,
+};
 use serde_json::json;
 
-use super::RouteHandler;
+use crate::{repository, route::AppState};
 
-#[derive(Serialize)]
-struct CourseResponse {
-    nama: String,
-    semester: u16,
-    sks: u16,
-}
+use super::{RouteHandler, SuccessResponse};
 
-pub async fn course_handler(Query(_params): Query<HashMap<String, String>>) -> RouteHandler {
-    let response = CourseResponse {
-        nama: String::from("jaringan komputer"),
-        semester: 5,
-        sks: 3,
+pub async fn course_handler(
+    State(state): State<Arc<AppState>>,
+    Query(params): Query<HashMap<String, String>>,
+) -> RouteHandler {
+    let course_repo = repository::course_repository::CourseRepository::new(&state.db_pool);
+    let mut courses = match course_repo.get_courses().await {
+        Ok(result) => result,
+        _ => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                String::from("Internal server error"),
+            ));
+        }
+    };
+
+    if let Some(course_name_param) = params.get("nama") {
+        courses.retain(|course| course.nama == *course_name_param)
+    }
+    if let Some(semester_param) = params.get("semester") {
+        let parse_semester_param = match semester_param.parse::<i8>() {
+            Ok(num) => num,
+            _ => {
+                return Err((
+                    StatusCode::BAD_REQUEST,
+                    String::from("semester wajib berupa angka"),
+                ))
+            }
+        };
+        courses.retain(|course| course.semester == parse_semester_param)
+    }
+    if let Some(sks_param) = params.get("sks") {
+        let parse_sks_param = match sks_param.parse::<i8>() {
+            Ok(num) => num,
+            _ => {
+                return Err((
+                    StatusCode::BAD_REQUEST,
+                    String::from("sks wajib berupa angka"),
+                ))
+            }
+        };
+        courses.retain(|course| course.sks == parse_sks_param)
+    }
+    let response = SuccessResponse {
+        total_results: courses.len(),
+        data: courses,
     };
 
     Ok(Json(json!(response)))
+}
+
+pub async fn course_with_id_handler(
+    State(state): State<Arc<AppState>>,
+    Path(id_matkul): Path<String>,
+) -> RouteHandler {
+    let course_repo = repository::course_repository::CourseRepository::new(&state.db_pool);
+    let course = match course_repo.get_course_by_id(&id_matkul).await {
+        Ok(course) => course,
+        Err(err) => match err {
+            sqlx::Error::RowNotFound => {
+                return Err((
+                    StatusCode::NOT_FOUND,
+                    String::from("Matkul tidak ditemukan"),
+                ))
+            }
+            _ => {
+                return Err((
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    String::from("Internal server error"),
+                ))
+            }
+        },
+    };
+
+    Ok(Json(json!(course)))
 }
