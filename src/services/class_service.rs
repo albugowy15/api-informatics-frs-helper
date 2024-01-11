33f7cf791c654a6e1ad052744c605574d74594d0
albugowy15 <kholidbughowi@gmail.com>
2024-01-11 11:21:44 +0700

    feat: use IntoJson to convert struct to json response

--- src/services/class_service.rs
+++ src/services/class_service.rs
@@ -1,25 +1,20 @@
 use std::{collections::HashMap, sync::Arc};
 
-use axum::{
-    extract::{Path, Query, State},
-    Json,
-};
-use serde_json::json;
+use axum::extract::{Path, Query, State};
 
-use crate::{repository, route::AppState};
+use crate::{repository::class_repository::ClassRepository, route::AppState};
 
-use super::{display_err, ErrorViews, JsonResponse, RouteHandler, SuccessResponse};
+use super::{display_err, DataResponse, ErrorViews, IntoJson, JsonResponse, RouteHandler};
 
 pub async fn classes(
-    State(app_state): State<Arc<AppState>>,
+    State(state): State<Arc<AppState>>,
     Query(params): Query<HashMap<String, String>>,
 ) -> RouteHandler<JsonResponse> {
-    let class_repo = repository::class_repository::ClassRepository::new(&app_state.db_pool);
+    let class_repo = ClassRepository::new(&state.db_pool);
     let mut classes = match class_repo.get_classes().await {
         Ok(classes) => classes,
         _ => return Err(display_err(ErrorViews::Internal)),
     };
-
     if let Some(day_param) = params.get("hari") {
         classes.retain(|x| x.hari.to_lowercase() == *day_param.to_lowercase());
     }
@@ -27,23 +22,23 @@ pub async fn classes(
         classes.retain(|x| x.jam == *hour_param);
     }
     if let Some(course_name_param) = params.get("matkul") {
-        classes.retain(|x| x.matkul.to_lowercase() == *course_name_param.to_lowercase());
+        classes.retain(|x| {
+            x.matkul
+                .to_lowercase()
+                .contains(&course_name_param.to_lowercase())
+        });
     }
     if let Some(lecturer_code_param) = params.get("kode_dosen") {
         classes.retain(|x| x.kode_dosen.to_lowercase() == *lecturer_code_param.to_lowercase());
     }
-    let response = SuccessResponse {
-        total_results: classes.len(),
-        data: classes,
-    };
-    Ok(Json(json!(response)))
+    Ok(DataResponse::new(classes.len(), classes).into_json())
 }
 
 pub async fn class_by_id(
-    State(app_state): State<Arc<AppState>>,
+    State(state): State<Arc<AppState>>,
     Path(id_kelas): Path<String>,
 ) -> RouteHandler<JsonResponse> {
-    let class_repo = repository::class_repository::ClassRepository::new(&app_state.db_pool);
+    let class_repo = ClassRepository::new(&state.db_pool);
     let class = match class_repo.get_class_by_id(&id_kelas).await {
         Ok(class) => class,
         Err(err) => match err {
@@ -53,5 +48,5 @@ pub async fn class_by_id(
             }
         },
     };
-    Ok(Json(json!(class)))
+    Ok(class.into_json())
 }
